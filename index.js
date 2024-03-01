7545a9db9f595c66961c3b835d82d6a040d41269
Uloma Okenyi <cyndibilo@gmail.com>
2024-03-01 06:28:21 +0000

    first version

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(" Uloma Okenyi ")
+#!/usr/bin/env node
+
+console.log(`Uloma Okenyi - Software Engineer
+
+Uloma Okenyi is a highly skilled Senior Software Engineer specializing in designing and developing sustainable software solutions using diverse programming languages and frameworks including ASP.Net Core, Spring Boot, and Node Js.
+
+She specializes in building web API and excels in translating complex business requirements into secure, efficient, and user-friendly applications.
+
+Twitter: https://twitter.com/Ulomaramma
+website: visionbling.com
+`)
